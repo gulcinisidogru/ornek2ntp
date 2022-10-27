0f7fce1e93105d8d9d7d12b20f51e14d67409eb5
gulci <gulci@LAPTOP-KPNR185M>
2022-10-27 14:50:49 +0300

    Proje dosyası ekle.

---
+++ ornek2ntp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek2ntp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 2;
+            int b = 3;
+            int c = 6;
+            Console.WriteLine("(a&b:):" + (a & b));
+            Console.WriteLine("(a|b:):" + (a | b));
+            Console.WriteLine("(a^b:):" + (a ^ b));
+            Console.WriteLine("(~a):" + (~a));
+            Console.WriteLine("(~b):" + (~b));
+            Console.WriteLine("(~c):" + (~c));
+
+
+
+
+
+
+        }
+    }
+}
